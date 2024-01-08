991540ff98f92aa3d2cd6058a12290d7585ae9c8
taynpg <taynpg@163.com>
2024-01-08 23:12:44 +0800

    版本号v1.0.3

--- MainWidget.cpp
+++ MainWidget.cpp
@@ -12,7 +12,7 @@ MainWidget::MainWidget(QWidget* parent)
     setFixedHeight(210);
     setMinimumWidth(910);
     setWindowIcon(QIcon("://resource/ico.ico"));
-    setWindowTitle(u8"快速创建code可用的cmake工程 v1.0.2");
+    setWindowTitle(u8"快速创建code可用的cmake工程 v1.0.3");
 
     init_ui();
     read_ini();
